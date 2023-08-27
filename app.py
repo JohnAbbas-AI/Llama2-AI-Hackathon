313f9d34854223ec625ceefecf5598c1975d52b5
Muhammad John Abbas <72137465+JohnAbbas-AI@users.noreply.github.com>
2023-08-27 06:53:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ import llama
 def clear_chat():
     st.session_state.messages = [{"role": "assistant", "content": "Say something to get started!"}]
 
-
 st.title("Good Doctor")
-
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Say something to get started!"}]
-
 
 with st.form("chat_input", clear_on_submit=True):
     a, b = st.columns([4, 1])
@@ -24,31 +21,17 @@ with st.form("chat_input", clear_on_submit=True):
 
     b.form_submit_button("Send", use_container_width=True)
 
-
-for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"] == "user")
-
+for index, msg in enumerate(st.session_state.messages):
+    message(msg["content"], is_user=msg["role"] == "user", key=f"message_{index}")
 
 if user_prompt:
-
-    print('user_prompt: ', user_prompt)
-
     st.session_state.messages.append({"role": "user", "content": user_prompt})
-    
-    message(user_prompt, is_user=True)
 
-    response = llama.get_response(user_prompt)  # get response from llama2 API (in our case from Workflow we created before)
+    response = llama.get_response(user_prompt)
 
     msg = {"role": "assistant", "content": response}
 
-    print('st.session_state.messages: ', st.session_state.messages)
-
     st.session_state.messages.append(msg)
-
-    print('msg.content: ', msg["content"])
-
-    message(msg["content"])
-
 
 if len(st.session_state.messages) > 1:
     st.button('Clear Chat', on_click=clear_chat)
